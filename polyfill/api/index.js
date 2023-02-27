e3e65023f47f245626e70b47ec73b995a9b624e1
Felix Zhang <fe1ix@fb.com>
2023-02-27 14:55:28 -0800

    fix(missing-global-classes): expose XRPlane and XRAnchor classes to global
    Summary: XRPlane and XRAnchor classes were not exposed to global scope before, this was fine with threejs, but Babylon.js checks the existence of these classes to determine whether browser environment is compatible.
    
    Reviewed By: cabanier
    
    Differential Revision:
    D43587132
    
    Privacy Context Container: L1164747
    
    fbshipit-source-id: e723c61142605d06757b6771370d36383d6eaee5

--- polyfill/api/index.js
+++ polyfill/api/index.js
@@ -1,13 +1,21 @@
+import { XRAnchor, XRAnchorSet } from './XRAnchor';
+import { XRPlane, XRPlaneOrientation, XRPlaneSet } from './XRPlane';
+
 import XRHitTestResult from './XRHitTestResult';
 import XRHitTestSource from './XRHitTestSource';
+import XRRay from './XRRay';
 import XRTransientInputHitTestResult from './XRTransientInputHitTestResult';
 import XRTransientInputHitTestSource from './XRTransientInputHitTestSource';
-import XRRay from './XRRay';
 
 export default {
-  XRHitTestResult,
-  XRHitTestSource,
-  XRTransientInputHitTestResult,
-  XRTransientInputHitTestSource,
-  XRRay
+	XRHitTestResult,
+	XRHitTestSource,
+	XRTransientInputHitTestResult,
+	XRTransientInputHitTestSource,
+	XRRay,
+	XRPlane,
+	XRPlaneSet,
+	XRAnchor,
+	XRAnchorSet,
+	XRPlaneOrientation,
 };
