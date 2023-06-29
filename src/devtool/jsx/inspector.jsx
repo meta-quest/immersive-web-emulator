8935074b4d2bf9e1db0bbc9ac7600055171d7522
Felix Zhang <fe1ix@meta.com>
2023-06-29 11:06:09 -0700

    feat(react-conversion): render room settings with react
    Summary: Add back room settings on top of 3D inspector, and convert it to using React
    
    Reviewed By: cabanier
    
    Differential Revision:
    D47113380
    
    Privacy Context Container: L1164747
    
    fbshipit-source-id: 4fc98888c394d64721a085f237fcbba8dd6c5776

--- src/devtool/jsx/inspector.jsx
+++ src/devtool/jsx/inspector.jsx
@@ -7,10 +7,14 @@
 
 import { DEVICE, OBJECT_NAME } from '../js/constants';
 
+import { EmulatorSettings } from '../js/emulatorStates';
 import React from 'react';
+import { changeRoomDimension } from '../js/messenger';
 
 export default function Inspector({ device }) {
 	const sceneContainerRef = React.useRef();
+	const [showTransforms, setShowTransforms] = React.useState(true);
+	const [showRoomSettings, setShowRoomSettings] = React.useState(false);
 	const transformData = {};
 	Object.values(DEVICE).forEach((deviceKey) => {
 		const deviceName = OBJECT_NAME[deviceKey];
@@ -40,51 +44,103 @@ export default function Inspector({ device }) {
 		<>
 			<div ref={sceneContainerRef} id="scene-container"></div>
 			<div id="transform-component">
-				{Object.values(DEVICE).map((deviceKey) => {
-					const deviceName = OBJECT_NAME[deviceKey];
-					return (
-						<div key={deviceKey} className="row transform-card">
-							<div className="col-2 transform-icon d-flex justify-content-center align-items-center">
-								<img
-									src={'./assets/images/' + deviceName + '.png'}
-									className="control-icon"
-								/>
-							</div>
-							<div className="col-10 transform-body">
-								<div className="row">
-									<div className="value">
-										<span>
-											{fixNumber(transformData[deviceName][0].position[0])}
-										</span>
-										&nbsp;
-										<span>
-											{fixNumber(transformData[deviceName][0].position[1])}
-										</span>
-										&nbsp;
-										<span>
-											{fixNumber(transformData[deviceName][0].position[2])}
-										</span>
+				<button
+					onClick={() => {
+						setShowTransforms(!showTransforms);
+						setShowRoomSettings(false);
+					}}
+					className={showTransforms ? 'active' : ''}
+				>
+					device
+				</button>
+				<button
+					onClick={() => {
+						setShowRoomSettings(!showRoomSettings);
+						setShowTransforms(false);
+					}}
+					className={showRoomSettings ? 'active' : ''}
+				>
+					room
+				</button>
+				{showTransforms &&
+					Object.values(DEVICE).map((deviceKey) => {
+						const deviceName = OBJECT_NAME[deviceKey];
+						return (
+							<div key={deviceKey} className="row transform-card">
+								<div className="col-2 transform-icon d-flex justify-content-center align-items-center">
+									<img
+										src={'./assets/images/' + deviceName + '.png'}
+										className="control-icon"
+									/>
+								</div>
+								<div className="col-10 transform-body">
+									<div className="row">
+										<div className="value">
+											<span>
+												{fixNumber(transformData[deviceName][0].position[0])}
+											</span>
+											&nbsp;
+											<span>
+												{fixNumber(transformData[deviceName][0].position[1])}
+											</span>
+											&nbsp;
+											<span>
+												{fixNumber(transformData[deviceName][0].position[2])}
+											</span>
+										</div>
+									</div>
+									<div className="row">
+										<div className="value">
+											<span>
+												{fixNumber(transformData[deviceName][0].rotation[0])}
+											</span>
+											&nbsp;
+											<span>
+												{fixNumber(transformData[deviceName][0].rotation[1])}
+											</span>
+											&nbsp;
+											<span>
+												{fixNumber(transformData[deviceName][0].rotation[2])}
+											</span>
+										</div>
 									</div>
 								</div>
-								<div className="row">
-									<div className="value">
-										<span>
-											{fixNumber(transformData[deviceName][0].rotation[0])}
-										</span>
-										&nbsp;
-										<span>
-											{fixNumber(transformData[deviceName][0].rotation[1])}
-										</span>
-										&nbsp;
-										<span>
-											{fixNumber(transformData[deviceName][0].rotation[2])}
-										</span>
-									</div>
-								</div>
 							</div>
+						);
+					})}{' '}
+				{showRoomSettings && (
+					<div className="row transform-card" id="room-dimension-settings">
+						<div className="col-2 transform-icon d-flex justify-content-center align-items-center">
+							<img
+								src="./assets/images/roomscale.png"
+								className="control-icon"
+							/>
 						</div>
-					);
-				})}
+						<div className="col-10 transform-body">
+							{[
+								['x', 'width'],
+								['y', 'height'],
+								['z', 'depth'],
+							].map(([key, name]) => (
+								<div key={name + key} className="row">
+									<span>Space {name}:</span>
+									<input
+										type="number"
+										defaultValue={EmulatorSettings.instance.roomDimension[key]}
+										onChange={(event) => {
+											EmulatorSettings.instance.roomDimension[key] = parseFloat(
+												event.target.value,
+											);
+											EmulatorSettings.instance.write();
+											device.updateRoom();
+											changeRoomDimension();
+										}}
+									/>
+								</div>
+							))}
+						</div>
+					</div>
+				)}
 			</div>
 		</>
 	);
